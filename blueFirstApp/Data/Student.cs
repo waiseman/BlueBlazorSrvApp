6e25e4ca39325ecd1c1f3ecd5bd5ffe7da4c8263
Museb <m.museb@m3logi.com>
2023-11-01 16:20:50 +0500

    11-1-2013 paramter -navigation -form binding

---
+++ blueFirstApp/Data/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace blueFirstApp.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
