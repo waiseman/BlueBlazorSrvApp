cf20ebe0ef2240445b220836c66d642090c6fafa
Museb <m.museb@m3logi.com>
2023-11-08 20:34:33 +0500

    --Form Data Binding

--- blueFirstApp/Data/Student.cs
+++ blueFirstApp/Data/Student.cs
@@ -1,9 +1,18 @@
-﻿namespace blueFirstApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blueFirstApp.Data
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Range(18,35,ErrorMessage ="User must be 18-35" )]
         public int Age { get; set; }
+
+        [Required]
+        [StringLength(40)]
         public string Name { get; set; }
         public string Description { get; set; }
 
